748ca086d8ba6deacb918ab39cf81c3466eef624
Marian Capote <mcapote250390@gmail.com>
2019-02-08 15:37:28 -0300

    first commit

---
+++ src/main/java/com/mydomain/clients/backend/apirest/ClientsBackendApirestApplication.java
@@ -0,0 +1,14 @@
+package com.mydomain.clients.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsBackendApirestApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/mydomain/clients/backend/apirest/models/entity/Client.java
@@ -0,0 +1,78 @@
+package com.mydomain.clients.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "client")
+
+//cuando la tabal de la BD se llama igual a la clase no es necesario usar la notacion Table para especificar el name
+
+public class Client implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	private String email;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+}
