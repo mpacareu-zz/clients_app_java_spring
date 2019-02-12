c6118fc195ce12fd2becf1f75a794d599a336d35
mcapote <mcapote250390@gmail.com>
2019-02-12 06:49:29 -0300

    bad request execptions in create and update

--- src/main/java/com/mydomain/clients/backend/apirest/controllers/ClientRestController.java
+++ src/main/java/com/mydomain/clients/backend/apirest/controllers/ClientRestController.java
@@ -1,13 +1,18 @@
 package com.mydomain.clients.backend.apirest.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -57,9 +62,16 @@ public class ClientRestController {
 	}
 	
 	@PostMapping("/clients")
-	public ResponseEntity<?> create (@RequestBody Client client) {
+	public ResponseEntity<?> create (@Valid @RequestBody Client client, BindingResult result) {
 		Client newClient= null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+			List<String> errors = result.getFieldErrors().stream().map(e-> "The field, '"+e.getField()+"' has the following error:"+e.getDefaultMessage()).collect(Collectors.toList());
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+		
 		try {
 			newClient =clientService.save(client);
 		} 
@@ -77,10 +89,16 @@ public class ClientRestController {
 	}
 	
 	@PutMapping("/clients/{id}")
-	public ResponseEntity<?> update (@RequestBody Client client, @PathVariable Long id) {
+	public ResponseEntity<?> update (@Valid @RequestBody Client client, @PathVariable Long id, BindingResult result) {
 		Client currentClient = null;
 		Client savedClient = null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+			List<String> errors = result.getFieldErrors().stream().map(e-> "The field, '"+e.getField()+"' has the following error:"+e.getDefaultMessage()).collect(Collectors.toList());
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
 		
 		try {
 			currentClient = clientService.findById(id);
